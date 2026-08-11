5f23c6e842d4f96dbdcdb7eafddf0fb7097ad18c
rohinishewalkar2003-pixel <rohinishewalkar2003@gmail.com>
2026-08-11 23:59:25 +0530

    387.First Unique character in string.java

--- 387.First Unique character in string.java
+++ 387.First Unique character in string.java
@@ -1,12 +1,12 @@
  class Solution {
     public int firstUniqChar(String s) {
-        int[] count = new int[22];
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for (char c : s.toCharArray())
-            count[c - 'a']++;
+            map.put(c, map.getOrDefault(c, 0) + 1);
 
         for (int i = 0; i < s.length(); i++)
-            if (count[s.charAt(i) - 'a'] == 1)
+            if (map.get(s.charAt(i)) == 1)
                 return i;
         return -1;
     }
